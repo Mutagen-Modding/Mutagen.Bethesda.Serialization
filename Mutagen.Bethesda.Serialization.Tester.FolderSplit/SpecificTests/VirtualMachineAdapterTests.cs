e395c11e72fd6b8dd0530f45784f437cf1f39749
Justin Swanson <justin.c.swanson@gmail.com>
2023-10-03 16:39:43 -0500

    Missing await on VirtualMachineAdapterTest

--- Mutagen.Bethesda.Serialization.Tester.FolderSplit/SpecificTests/VirtualMachineAdapterTests.cs
+++ Mutagen.Bethesda.Serialization.Tester.FolderSplit/SpecificTests/VirtualMachineAdapterTests.cs
@@ -10,13 +10,13 @@ namespace Mutagen.Bethesda.Serialization.Tester.FolderSplit.SpecificTests;
 public class VirtualMachineAdapterTests
 {
     [Fact]
-    public void Test()
+    public async Task Test()
     {
         var groupRecStreamPackage = new StreamPackage(File.OpenRead("Files/VirtualMachineAdapterTestData.json"), null!);
         var kernel = new NewtonsoftJsonSerializationReaderKernel();
         var dataWriter = kernel.GetNewObject(groupRecStreamPackage);
         var metaData = new SerializationMetaData(GameRelease.SkyrimSE, new InlineWorkDropoff(), IFileSystemExt.DefaultFilesystem, NormalFileStreamCreator.Instance, CancellationToken.None);
-        var a = Activator_Serialization.Deserialize(dataWriter, kernel, metaData);
+        var a = await Activator_Serialization.Deserialize(dataWriter, kernel, metaData);
         // ToDo
         // Maybe add specific property checks
         // Was just added as there was an exception
