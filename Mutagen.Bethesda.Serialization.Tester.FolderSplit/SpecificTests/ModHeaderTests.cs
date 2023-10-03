effb6bcfd66a53fea855de9777550af245b51222
Justin Swanson <justin.c.swanson@gmail.com>
2023-10-03 16:39:48 -0500

    ModHeaderTests

---
+++ Mutagen.Bethesda.Serialization.Tester.FolderSplit/SpecificTests/ModHeaderTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Mutagen.Bethesda.Serialization.Streams;
+using Mutagen.Bethesda.Serialization.Yaml;
+using Mutagen.Bethesda.Skyrim;
+using Noggog;
+using Noggog.IO;
+using Noggog.WorkEngine;
+
+namespace Mutagen.Bethesda.Serialization.Tester.FolderSplit.SpecificTests;
+
+public class ModHeaderTests
+{
+    [Fact]
+    public async Task Test()
+    {
+        var groupRecStreamPackage = new StreamPackage(File.OpenRead("Files/ModHeader.yaml"), null!);
+        var kernel = new YamlSerializationReaderKernel();
+        var dataWriter = kernel.GetNewObject(groupRecStreamPackage);
+        var metaData = new SerializationMetaData(GameRelease.SkyrimSE, new InlineWorkDropoff(), IFileSystemExt.DefaultFilesystem, NormalFileStreamCreator.Instance, CancellationToken.None);
+        var a = await SkyrimModHeader_Serialization.Deserialize(dataWriter, kernel, metaData);
+        a.Stats.NumRecords.Should().Be(0);
+        a.Stats.NextFormID.Should().Be(0x800);
+    }
+}
