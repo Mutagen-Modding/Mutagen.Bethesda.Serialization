408012ed43c1fae3d66710ddb5ee2cc8c0011284
Justin Swanson <justin.c.swanson@gmail.com>
2024-10-22 01:39:22 -0500

    Missing test

--- Mutagen.Bethesda.Serialization.Tests/MemberTests/MemberTests.ArraySliceGetter#SomeObject_Serializations.g.verified.cs
+++ Mutagen.Bethesda.Serialization.Tests/MemberTests/MemberTests.ArraySliceGetter#SomeObject_Serializations.g.verified.cs
@@ -1,1 +1,183 @@
-﻿
+﻿//HintName: SomeObject_Serializations.g.cs
+using Loqui;
+using Mutagen.Bethesda.Plugins;
+using Mutagen.Bethesda.Serialization;
+using Mutagen.Bethesda.Serialization.Exceptions;
+using Mutagen.Bethesda.Serialization.SourceGenerator.Tests;
+using Mutagen.Bethesda.Serialization.Streams;
+using Mutagen.Bethesda.Serialization.Utility;
+using Noggog;
+using Noggog.IO;
+using Noggog.WorkEngine;
+using System.IO.Abstractions;
+using System.Threading.Tasks;
+
+#nullable enable
+
+#pragma warning disable CS1998 // No awaits used
+#pragma warning disable CS0618 // Obsolete
+
+namespace Mutagen.Bethesda.Serialization.SourceGenerator.Tests;
+
+internal static class SomeObject_Serialization
+{
+    public static async Task Serialize<TKernel, TWriteObject>(
+        TWriteObject writer,
+        Mutagen.Bethesda.Serialization.SourceGenerator.Tests.ISomeObjectGetter item,
+        MutagenSerializationWriterKernel<TKernel, TWriteObject> kernel,
+        SerializationMetaData metaData)
+        where TKernel : ISerializationWriterKernel<TWriteObject>, new()
+        where TWriteObject : IContainStreamPackage
+    {
+        metaData.Cancel.ThrowIfCancellationRequested();
+        await SerializeFields<TKernel, TWriteObject>(
+            writer: writer,
+            item: item,
+            kernel: kernel,
+            metaData: metaData);
+    }
+
+    public static async Task SerializeFields<TKernel, TWriteObject>(
+        TWriteObject writer,
+        Mutagen.Bethesda.Serialization.SourceGenerator.Tests.ISomeObjectGetter item,
+        MutagenSerializationWriterKernel<TKernel, TWriteObject> kernel,
+        SerializationMetaData metaData)
+        where TKernel : ISerializationWriterKernel<TWriteObject>, new()
+        where TWriteObject : IContainStreamPackage
+    {
+        metaData.Cancel.ThrowIfCancellationRequested();
+        if (item.SomeArray is {} checkedSomeArray
+            && checkedSomeArray.Length > 0)
+        {
+            kernel.StartListSection(writer, "SomeArray");
+            foreach (var listItem in checkedSomeArray)
+            {
+                kernel.WriteFloat(writer, null, listItem, default(float), checkDefaults: false);
+            }
+            kernel.EndListSection(writer);
+        }
+        if (item.SomeArray2 is {} checkedSomeArray2)
+        {
+            kernel.StartListSection(writer, "SomeArray2");
+            foreach (var listItem in checkedSomeArray2)
+            {
+                kernel.WriteFloat(writer, null, listItem, default(float), checkDefaults: false);
+            }
+            kernel.EndListSection(writer);
+        }
+        if (item.SomeArray3 is {} checkedSomeArray3
+            && checkedSomeArray3.Length > 0)
+        {
+            kernel.StartListSection(writer, "SomeArray3");
+            foreach (var listItem in checkedSomeArray3)
+            {
+                kernel.WriteFloat(writer, null, listItem, default(float), checkDefaults: false);
+            }
+            kernel.EndListSection(writer);
+        }
+    }
+
+    public static bool HasSerializationItems(
+        Mutagen.Bethesda.Serialization.SourceGenerator.Tests.ISomeObjectGetter? item,
+        SerializationMetaData metaData)
+    {
+        metaData.Cancel.ThrowIfCancellationRequested();
+        if (item == null) return false;
+        if (item.SomeArray.Length > 0) return true;
+        if (item.SomeArray2?.Length > 0) return true;
+        if (item.SomeArray3.Length > 0) return true;
+        return false;
+    }
+
+    public static async Task<Mutagen.Bethesda.Serialization.SourceGenerator.Tests.SomeObject> Deserialize<TReadObject>(
+        TReadObject reader,
+        ISerializationReaderKernel<TReadObject> kernel,
+        SerializationMetaData metaData)
+        where TReadObject : IContainStreamPackage
+    {
+        metaData.Cancel.ThrowIfCancellationRequested();
+        var obj = new Mutagen.Bethesda.Serialization.SourceGenerator.Tests.SomeObject();
+        await DeserializeInto<TReadObject>(
+            reader: reader,
+            kernel: kernel,
+            obj: obj,
+            metaData: metaData);
+        return obj;
+    }
+
+    public static async Task DeserializeSingleFieldInto<TReadObject>(
+        TReadObject reader,
+        ISerializationReaderKernel<TReadObject> kernel,
+        Mutagen.Bethesda.Serialization.SourceGenerator.Tests.ISomeObject obj,
+        SerializationMetaData metaData,
+        string name)
+        where TReadObject : IContainStreamPackage
+    {
+        metaData.Cancel.ThrowIfCancellationRequested();
+        switch (name)
+        {
+            case "SomeArray":
+                {
+                    obj.SomeArray = await SerializationHelper.ReadArray<ISerializationReaderKernel<TReadObject>, TReadObject, float>(
+                        reader: reader,
+                        kernel: kernel,
+                        metaData: metaData,
+                        itemReader: static async (r, k, m) =>
+                        {
+                            return SerializationHelper.StripNull(k.ReadFloat(r), name: "SomeArray");
+                        });
+                }
+                break;
+            case "SomeArray2":
+                {
+                    obj.SomeArray2 = await SerializationHelper.ReadArray<ISerializationReaderKernel<TReadObject>, TReadObject, float>(
+                        reader: reader,
+                        kernel: kernel,
+                        metaData: metaData,
+                        itemReader: static async (r, k, m) =>
+                        {
+                            return SerializationHelper.StripNull(k.ReadFloat(r), name: "SomeArray2");
+                        });
+                }
+                break;
+            case "SomeArray3":
+                {
+                    await SerializationHelper.ReadIntoArray<ISerializationReaderKernel<TReadObject>, TReadObject, float>(
+                        reader: reader,
+                        arr: obj.SomeArray3,
+                        kernel: kernel,
+                        metaData: metaData,
+                        itemReader: static async (r, k, m) =>
+                        {
+                            return SerializationHelper.StripNull(k.ReadFloat(r), name: "SomeArray3");
+                        });
+                }
+                break;
+            default:
+                kernel.Skip(reader);
+                break;
+        }
+    }
+    
+    public static async Task DeserializeInto<TReadObject>(
+        TReadObject reader,
+        ISerializationReaderKernel<TReadObject> kernel,
+        Mutagen.Bethesda.Serialization.SourceGenerator.Tests.ISomeObject obj,
+        SerializationMetaData metaData)
+        where TReadObject : IContainStreamPackage
+    {
+        metaData.Cancel.ThrowIfCancellationRequested();
+        while (kernel.TryGetNextField(reader, out var name))
+        {
+            await DeserializeSingleFieldInto<TReadObject>(
+                reader: reader,
+                kernel: kernel,
+                obj: obj,
+                metaData: metaData,
+                name: name);
+        }
+
+    }
+
+}
+
